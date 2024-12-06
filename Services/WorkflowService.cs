13d3c2830d54d5eac5b79714df7bb5a8a8e4aa5f
justindstein <jaydstein@gmail.com>
2024-12-06 10:47:35 -0500

    Working create order.

--- Services/WorkflowService.cs
+++ Services/WorkflowService.cs
@@ -1,12 +1,7 @@
 ﻿using learning_asp_core.Controllers;
 using learning_asp_core.Data;
 using learning_asp_core.Models.Requests.Inbound;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Net.Http;
-using System.Xml.Linq;
+using System.Text;
 
 namespace learning_asp_core.Services
 {
@@ -25,24 +20,35 @@ namespace learning_asp_core.Services
             _httpClient = httpClientFactory.CreateClient("retryClient");
             _configuration = configuration;
             _appDbContext = appDbContext;
+
+            // Retrieve credentials from configuration
+            string? username = _configuration["Azure:Username"];
+            string? password = _configuration["Azure:Password"];
+
+            // Encode credentials in Base64
+            var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"justin.stein@newwavena.com:G7Xkgrag4kCA0kV55aaZpA5Kpt2RIeibcnsFtdVhHk3qIenhcwkJJQQJ99AKACAAAAAw6rZpAAASAZDO9hta"));
+
+            // Add Authorization header
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", credentials);
         }
 
         public async void OpenWorkflow(OpenWorkflowRequest openWorkflowRequest)
         {
-            var workflows = _appDbContext.Workflows.ToList();
-            foreach (var workflow in workflows) 
-            {
-                _logger.LogInformation(workflow.Name); 
-            }
+            // Convert the request object to JSON
+            //string requestBody = System.Text.Json.JsonSerializer.Serialize(openWorkflowRequest);
+            string requestBody = openWorkflowRequest.ToCreateOrderWorkItemRequest().ToRequestBody();
+            _logger.LogInformation("requestBody: " + requestBody);
 
-           // convert OpenWorkflowRequest to API request object for azure devops
-           // send message to devops
+            // Create an HTTP content object
+            HttpContent content = new StringContent(requestBody, Encoding.UTF8, "application/json-patch+json");
 
-           HttpResponseMessage response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
-            response.EnsureSuccessStatusCode();
+            // convert OpenWorkflowRequest to API request object for azure devops
+            // send message to devops
+            HttpResponseMessage response = await _httpClient.PostAsync("https://dev.azure.com/aheadapparel/order-workflow/_apis/wit/workitems/$Order?api-version=7.1&$expand=all", content);
+            // response.EnsureSuccessStatusCode();
 
             string responseBody = await response.Content.ReadAsStringAsync();
-            //_logger.LogInformation("Status Code: {StatusCode} Response Body: {ResponseBody}", response.StatusCode, responseBody); 
+            _logger.LogInformation("Status Code: {StatusCode} Response Body: {ResponseBody}", response.StatusCode, responseBody); 
         }
 
         public async void CloseWorkflow(CloseWorkflowRequest closeWorkflowRequest)
