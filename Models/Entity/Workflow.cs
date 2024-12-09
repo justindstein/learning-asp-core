a114708d4d3f301cdb0f5a3b4be88ac865265cb7
justindstein <jaydstein@gmail.com>
2024-12-09 13:51:41 -0500

    Refactor Workflow table.

--- Models/Entity/Workflow.cs
+++ Models/Entity/Workflow.cs
@@ -1,12 +1,12 @@
 ﻿namespace learning_asp_core.Models.Entity
 {
-        public class Workflow
-        {
-            public int WorkflowID { get; set; }
-            public int WorkItemID { get; set; }
-            public string WorkItemUrl { get; set; }
-            public string WorkItemType { get; set; }
-            public bool IsClosed { get; set; }
-            public string Data { get; set; }
-        }
+    public class Workflow
+    {
+        public int WorkflowID { get; set; }
+        public int WorkItemID { get; set; }
+        public string WorkItemUrl { get; set; }
+        public string WorkItemType { get; set; }
+        public bool IsClosed { get; set; }
+        public string Data { get; set; }
+    }
 }
